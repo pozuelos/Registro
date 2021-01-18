104556075d7efcdc371c03ffbfdc06e0987c2a19
pozuelos <70779646+pozuelos@users.noreply.github.com>
2021-01-18 14:11:02 -0600

    Add files via upload

--- js/programa.js
+++ js/programa.js
@@ -1,1 +1,22 @@
-alert("AVISO")
+//ESTO SE UTILIZA PARA DESPLEGAR UNA VENTANA EMERGENTE
+//EMERGENTE CON UN MENSAJE
+
+//alert("YA ESTA CONECTADO CON JAVASCRIPT")
+
+
+//variables con tipos de datos
+var datoTexto = "Hola"
+var datoNumerico = 2021
+var datoDecimal = 3.1416
+var dataChar = "A"
+var datosLogicos = true //o false
+
+
+//UTILIZANDO UN ALERT PARA MOSTRAR LOS TIPOS DE DATOS 
+//CON VARIABLES
+alert(datoTexto + " " + datoNumerico)
+
+
+
+//alert(datoNumerico + datoDecimal)
+
